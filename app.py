faeee57aca1c064a964060b659c1eb3127a7d8ab
dhanihaneesha <142230982+dhanihaneesha@users.noreply.github.com>
2025-10-08 10:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 
-import os
-print("hello world")
-print("current directory: ",os.getcwd())
-print(os.listdir())
+# import os
+# print("hello world")
+# print("current directory: ",os.getcwd())
+# print(os.listdir())
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello, World!"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
+
